54a16aa2f4bdbc02ed5f4d3f4d643420fdc22f12
kieuduykhanh <khanhkieu167@gmail.com>
2020-09-29 08:47:59 +0700

    commit face detect

--- studyWithAIVietNam/studyOpenCV/study1.py
+++ studyWithAIVietNam/studyOpenCV/study1.py
@@ -1,3 +1,5 @@
+import math
+
 def caculator_mean(numbers):
     return sum(numbers)/len(numbers)
 
@@ -6,5 +8,18 @@ def caculator_variance(numbers):
     numbers_ = list(map(lambda x: (x - mean)**2, numbers))
     return sum(numbers_)/len(numbers_)
 
-print(caculator_variance([5,3,6,7,4]))
-print('sdhuisahdh')
+lis1 = [7,18,29,2,10,9,9]
+lis2 = [1,6,12,8,6,21,10]
+
+
+def caculator_corraelation(lis1, lis2):
+    # mean1 = caculator_mean(lis1)
+    # mean2 = caculator_mean(lis2)
+    sumq = 0
+    for i in range(len(lis1)):
+        sumq += lis1[i] * lis2[i]
+    return (len(lis1)*sumq - sum(lis1)*sum(lis2))/\
+           (math.sqrt(len(lis1)*sum(list(map(lambda x: x**2, lis1))) - sum(lis1)**2)*
+            math.sqrt(len(lis2)*sum(list(map(lambda x: x**2, lis2))) - sum(lis2)**2))
+
+print(caculator_corraelation(lis1, lis2))
---
+++ studyWithAIVietNam/studyOpenCV/study5.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+import math
+from scipy.ndimage.filters import generic_filter
+
+lis1 = [4,3,5,2,7,4,2,7,4]
+lis2 = [5,4,6,2,7,7,3,2,5]
+
+
